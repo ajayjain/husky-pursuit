c6af705c541a704068dfde2281b37b28822af6d5
Ajay Jain <ajayjain318@gmail.com>
2014-06-17 15:31:40 -0700

    Finish writing phidget_driver.py. Health is published to /status/health and fire commands can be issued to /status/fire

---
+++ src/documentation/install.bash
@@ -0,0 +1,9 @@
+sudo apt-get install ros-hydro-husky-robot
+# sudo apt-get install ros-hydro-husky-desktop
+rosrun husky_bringup install
+
+sudo apt-get install ros-hydro-usb-cam
+
+sudo apt-get install libusb-1.0-0-dev
+
+# sudo apt-get install ros-hydro-laser-scan-matcher
