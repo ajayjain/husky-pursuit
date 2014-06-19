c67580c9012bcc0a989651c84e5839b246c3c05b
Ajay Jain <ajayjain318@gmail.com>
2014-06-19 14:51:41 -0700

    gmapping launch file

---
+++ husky_launcher.sh
@@ -0,0 +1,10 @@
+export ROS_ROOT=/home/ros/catkin_ws
+export PATH=$ROS_ROOT/bin:$PATH
+
+export ROS_MASTER_URI=http://192.168.1.125:11311
+export ROS_HOSTNAME=192.168.1.125
+
+#if [ $(tty) == "/dev/tty2" ]; then
+#	echo "Automatically logged in"
+#	roslaunch husky_base base.launch port:=/dev/ttyUSB0
+#fi
