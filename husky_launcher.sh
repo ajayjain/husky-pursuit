bca1dbfa13297827a4bd62188ee6a1d0ffb55b4f
Ajay Jain <ajayjain318@gmail.com>
2014-06-24 09:36:05 -0700

    Edit husky_launcher

--- husky_launcher.sh
+++ husky_launcher.sh
@@ -2,7 +2,7 @@ export ROS_ROOT=/home/ros/catkin_ws
 export PATH=$ROS_ROOT/bin:$PATH
 
 export ROS_MASTER_URI=http://192.168.1.125:11311
-export ROS_HOSTNAME=192.168.1.125
+# export ROS_HOSTNAME=192.168.1.125
 
 #if [ $(tty) == "/dev/tty2" ]; then
 #	echo "Automatically logged in"
