70c56977c523ac1bd3de48eca0c520907720c5c4
Ajay Jain <ajayjain318@gmail.com>
2014-06-25 10:13:10 -0700

    Fix hostname in husky_launcher

--- husky_launcher.sh
+++ husky_launcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Ajay Jain
+# This file is to bootstrap the environment of
+# the robot computer, not a workstation.
 
 export ROS_WS=/home/ros/catkin_ws
 
@@ -10,7 +12,7 @@ export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 
 export ROS_MASTER_URI=http://192.168.1.125:11311
-# export ROS_HOSTNAME=192.168.1.125
+export ROS_HOSTNAME=192.168.1.125
 
 #if [ $(tty) == "/dev/tty2" ]; then
 #	echo "Automatically logged in"
